d0cd3babcd7c9181406ecd5274bd3a1b30239359
David Göransson <david.goransson90@gmail.com>
2020-07-19 23:46:37 +0200

    Update lastEditedAt on update of note

--- app/src/main/java/com/rawa/notes/usecases/UpdateNoteUseCase.kt
+++ app/src/main/java/com/rawa/notes/usecases/UpdateNoteUseCase.kt
@@ -2,9 +2,10 @@ package com.rawa.notes.usecases
 
 import com.rawa.notes.domain.Note
 import com.rawa.notes.repository.NotesRepository
+import java.time.OffsetDateTime
 
 class UpdateNoteUseCase(private val repo: NotesRepository) {
     suspend fun execute(note: Note) {
-        return repo.updateNote(note)
+        return repo.updateNote(note.copy(lastEditedAt = OffsetDateTime.now()))
     }
 }
