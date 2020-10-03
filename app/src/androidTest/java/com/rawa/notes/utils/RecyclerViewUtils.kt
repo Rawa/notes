47144d111f85120942c99de1d7f78e267a815c39
David Göransson <david.goransson90@gmail.com>
2020-10-04 01:33:41 +0200

    Add test for checking deletion of note

--- app/src/androidTest/java/com/rawa/notes/utils/RecyclerViewUtils.kt
+++ app/src/androidTest/java/com/rawa/notes/utils/RecyclerViewUtils.kt
@@ -27,3 +27,25 @@ fun childMatcher(childId: Int, position: Int, childMatcher: Matcher<View>): Matc
         }
     }
 }
+
+fun hasItem(matcher: Matcher<View>): Matcher<View> {
+    return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
+        override fun describeTo(description: Description) {
+            description.appendText("has item: ")
+            matcher.describeTo(description)
+        }
+
+        override fun matchesSafely(view: RecyclerView): Boolean {
+            val adapter = view.adapter
+            for (position in 0 until adapter!!.itemCount) {
+                val type = adapter.getItemViewType(position)
+                val holder = adapter.createViewHolder(view, type)
+                adapter.onBindViewHolder(holder, position)
+                if (matcher.matches(holder.itemView)) {
+                    return true
+                }
+            }
+            return false
+        }
+    }
+}
